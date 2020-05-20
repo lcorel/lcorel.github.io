b745e1b8a2ca1ff67db2d011686eb6fc47ca6fe2
lcorel <cd.lp@qq.com>
2020-05-20 12:36:36 +0800

    Site updated: 2020-05-20 12:36:34

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -34,7 +34,8 @@ if (screen.width >= 768) {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
-			apiPath: "https://api.zdl.one/live2d/"
+			//apiPath: "https://api.zdl.one/live2d/"
+			apiPath: "http://blog.zzonn.com:9010/"
 		});
 	});
 }
